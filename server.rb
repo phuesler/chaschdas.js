28da3bb8074f2864ffdc8773c779182007e5abf6
Patrick Huesler <patrick.huesler@gmail.com>
2011-10-09 20:00:15 +0200

    generic server for status codes

--- server.rb
+++ server.rb
@@ -5,3 +5,27 @@ set :port, 3000
 get '/' do
   redirect '/index.html'
 end
+
+get "/get/:code" do
+  status params[:code]
+end
+
+post "/post/:code" do
+  status params[:code]
+end
+
+put "/put/:code" do
+  status params[:code]
+end
+
+delete "/delete/:code" do
+  status params[:code]
+end
+
+patch "/patch/:code" do
+  status params[:code]
+end
+
+options "/options/:code" do
+  status params[:code]
+end
