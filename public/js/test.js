44e0d9e446d4a4de66c94fdcaf579d64c188190b
Patrick Huesler <patrick.huesler@gmail.com>
2011-10-09 20:45:04 +0200

    label tests correctly

--- public/js/test.js
+++ public/js/test.js
@@ -1,6 +1,6 @@
 // Let's test this function
 
-asyncTest('asynchronous test', function() {
+asyncTest('/get/200', function() {
 	// The test is automatically paused
   $.ajax({
     url: 'http://cors.chaschdasjs.com:3000/get/200',
